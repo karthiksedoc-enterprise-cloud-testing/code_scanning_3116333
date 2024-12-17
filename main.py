74f065ab894450ffc359d236aa467b46b226a12b
Karthikeyan Balasubramaniam <40568655+karthiksedoc@users.noreply.github.com>
2024-12-17 17:02:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from django.db import connection
+import os
+from flask import Flask, request
+app = Flask(__name__)
 
-def show_user(request, username):
-    with connection.cursor() as cursor:
-    cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
+# curl -X GET "http://localhost:5000/tainted7/touch%20HELLO"
+@app.route("/tainted7/<something>")
+def test_sources_7(something):
+    
+    os.system(request.remote_addr) 
+
+    return "foo"
+
+if __name__ == "__main__":
+	app.run(debug=True) 
